adcb15c14096e014d378bbede94a2b5121a6c6f1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-11 11:43:37 +0900

    Fix

--- turbopack/crates/turbopack-ecmascript/src/references/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/mod.rs
@@ -1776,7 +1776,11 @@ async fn handle_call<G: Fn(Vec<Effect>) + Send + Sync>(
                     show_dynamic_warning = true;
                 }
                 if !dynamic || !ignore_dynamic_requests {
-                    analysis.add_reference(FileSourceReference::new(source, Pattern::new(pat)));
+                    analysis.add_reference(
+                        FileSourceReference::new(source, Pattern::new(pat))
+                            .to_resolved()
+                            .await?,
+                    )
                 }
                 if show_dynamic_warning {
                     let (args, hints) = explain_args(&args);
@@ -1817,12 +1821,16 @@ async fn handle_call<G: Fn(Vec<Effect>) + Send + Sync>(
                         return Ok(());
                     }
                 }
-                analysis.add_reference(CjsAssetReference::new(
-                    origin,
-                    Request::parse(Value::new(pat)),
-                    issue_source(source, span),
-                    in_try,
-                ));
+                analysis.add_reference(
+                    CjsAssetReference::new(
+                        origin,
+                        Request::parse(Value::new(pat)),
+                        issue_source(source, span),
+                        in_try,
+                    )
+                    .to_resolved()
+                    .await?,
+                );
                 return Ok(());
             }
             let (args, hints) = explain_args(&args);
